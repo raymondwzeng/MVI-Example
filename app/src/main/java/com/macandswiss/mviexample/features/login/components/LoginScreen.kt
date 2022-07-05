81a16b63830a85665964f902a67203289700fda5
raymondwzeng <rzeng3@gmail.com>
2022-07-04 23:23:43 -0700

    Finished first pass of MVI Login Screen

--- app/src/main/java/com/macandswiss/mviexample/features/login/components/LoginScreen.kt
+++ app/src/main/java/com/macandswiss/mviexample/features/login/components/LoginScreen.kt
@@ -1,4 +1,106 @@
 package com.macandswiss.mviexample.features.login.components
 
-class LoginScreen {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import com.macandswiss.mviexample.features.login.mvi.LoginState
+import com.macandswiss.mviexample.ui.theme.MVIExampleTheme
+import kotlinx.coroutines.flow.collect
+
+/**
+ * The actual screen of the login feature. Takes a view model and renders based on its state
+ *
+ * Question: Is this normally a fragment? An activity?
+ * Well, let's think about it this way - will a login be a standalone entity, or could it be part of a greater activity?
+ * I think for now it makes sense to have it as a standalone
+ */
+class LoginScreen : ComponentActivity() {
+    private lateinit var viewModel: LoginViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //Create a new instance or get the instance of the viewmodel(?) and assign its lifecycle owner to this
+        viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.currentState.collect { state: LoginState ->
+                setContent {
+                    Login(
+                        state = state,
+                        onLoginValueChanged = { viewModel.LoginChanged(newLogin = it) },
+                        onPasswordValueChanged = { viewModel.PasswordChanged(newPassword = it) },
+                        onSignInPressed = { viewModel.LoginStarted() }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Login(
+    state: LoginState,
+    onLoginValueChanged: (String) -> Unit = {},
+    onPasswordValueChanged: (String) -> Unit = {},
+    onSignInPressed: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        if (state.progressBar) {
+            CircularProgressIndicator(modifier = modifier)
+        } else {
+            Text(
+                text = "Login",
+                style = MaterialTheme.typography.h4
+            )
+            TextField(
+                value = state.login,
+                onValueChange = onLoginValueChanged,
+                maxLines = 1
+            )
+            Text(
+                text = "Password",
+                style = MaterialTheme.typography.h4
+            )
+            TextField(
+                value = state.password,
+                onValueChange = onPasswordValueChanged,
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                maxLines = 1
+            )
+            Divider(modifier = Modifier.size(16.dp), color = Color.Transparent)
+            Button(onClick = onSignInPressed) {
+                Text(text = "Log In")
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewLoginStandard() {
+    MVIExampleTheme {
+        Login(LoginState())
+    }
 }
--- app/src/main/java/com/macandswiss/mviexample/features/login/components/LoginViewModel.kt
+++ app/src/main/java/com/macandswiss/mviexample/features/login/components/LoginViewModel.kt
@@ -1,4 +1,33 @@
 package com.macandswiss.mviexample.features.login.components
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+import com.macandswiss.mviexample.features.login.mvi.LoginAction
+import com.macandswiss.mviexample.features.login.mvi.LoginReducer
+import com.macandswiss.mviexample.features.login.mvi.LoginState
+import com.macandswiss.mviexample.mvi.Store
+
+class LoginViewModel : ViewModel() {
+    //The "CPU" for our data cycle, delegating state management to something which isn't the VM.
+    private val _store = Store(
+        initialState = LoginState(),
+        reducer = LoginReducer()
+    )
+
+    //Exposed so that the View knows what to do
+    val currentState = _store.state
+
+    //Proxy functions that end up calling the store
+    fun LoginChanged(newLogin: String) {
+        _store.dispatch(LoginAction.LoginChanged(newLogin))
+    }
+
+    fun PasswordChanged(newPassword: String) {
+        _store.dispatch(LoginAction.PasswordChanged(newPassword))
+    }
+
+    fun LoginStarted() {
+        _store.dispatch(LoginAction.LoginStarted)
+    }
+
+    //We don't need to create proxies for LoginCompleted and LoginErrored because we do not want the user to perform these actions
 }
